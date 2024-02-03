0bd31596a5e160d06921bdd91d7b6af5c322a180
Soham Samanta <soham@Sohams-MacBook-Air-3.local>
2024-02-03 17:51:49 +0530

    few final touch

--- Snipe-Assignment/src/ProductList.jsx
+++ Snipe-Assignment/src/ProductList.jsx
@@ -29,23 +29,23 @@ export default function ProductList() {
     //   ))}
     // </div>
 
-    <div className="bg-pink-100 h-screen p-8">
-      <h1 className="text-gray-700 text-left"> Snipe Interview Assignment !</h1>
+    <div className="bg-pink-100 h-screen p-12">
+      <h1 className="text-gray-700 text-left"> Snipe Interview Assignment</h1>
 
-      <br></br>
+      <br></br><br></br>
 
       {products.map((x) => (
         <div key={x.id} className="bg-gray-500 p-4 rounded-lg flex mb-6">
           <div className="w-2/3">
             <h3 className="text-xl font-bold mb-2 text-left">{x.title}</h3>
-            <p className="text-gray-700 text-left">{x.description}</p>
+            <p className="text-white-700 text-left">{x.description}</p>
           </div>
 
           <div className="w-1/3 flex justify-end">
             <button className="bg-transparent border border-yellow-500 text-white-500 px-4 py-2 rounded">
               Add to cart
             </button>
-            &nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <button className="bg-transparent border border-yellow-500 text-white-500 px-4 py-2 rounded">
               Buy Now and Pay Later
             </button>
